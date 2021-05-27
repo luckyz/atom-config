d00c082d315f30b13ebde32fa4d1db1428cfb92c
Lucas Zurverra <lzurverra@gmail.com>
2021-05-27 02:15:35 -0300

    added packages access test

--- tests/test_packages_file_exists.py
+++ tests/test_packages_file_exists.py
@@ -1,10 +1,18 @@
 import pytest
 from pathlib import Path
+import json
 import os
 
 PACKAGES_FILENAME = "packages.json"
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
+PACKAGES_FULLPATH = os.path.join(BASE_DIR, PACKAGES_FILENAME)
+
 
 def test_packages_file_exists():
-    assert os.path.exists(os.path.join(BASE_DIR, PACKAGES_FILENAME)) == True
+    assert os.path.exists(PACKAGES_FULLPATH) is True
+
+
+def test_packages_access():
+    with open(PACKAGES_FULLPATH, "r") as file:
+        source = json.load(file)
+        assert source["packages"][0] == "atom-beautify"
