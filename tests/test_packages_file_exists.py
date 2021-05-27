a7b687f9565565ee569da449b2f4bbdca811af64
Lucas Zurverra <lzurverra@gmail.com>
2021-05-27 01:48:41 -0300

    fix packages.json get path

--- tests/test_packages_file_exists.py
+++ tests/test_packages_file_exists.py
@@ -1,8 +1,10 @@
 import pytest
+from pathlib import Path
 import os
 
 PACKAGES_FILENAME = "packages.json"
-BASE_DIR = os.path.dirname(__file__)
+BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 
 def test_packages_file_exists():
     assert os.path.exists(os.path.join(BASE_DIR, PACKAGES_FILENAME)) == True
